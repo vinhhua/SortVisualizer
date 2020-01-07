dcf4f8b0a119cd66f7414b95e7a1dd4f685740a8
huaphuvinh <huaphuvinh@gmail.com>
2020-01-06 18:48:00 -0800

    The menu screen can be improved, work on the sort class also

--- src/com/VinhHua/MenuScreen.java
+++ src/com/VinhHua/MenuScreen.java
@@ -1,34 +1,131 @@
 package com.VinhHua;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 public class MenuScreen extends JFrame{
-    private JPanel panel;
-    private JButton startButton;
-    private JButton resetButton;
+
+    private final static int WIDTH = 800;
+    private final static int HEIGHT = 500;
+    private final String[] sortAlgos = {"Bubble Sort", "Selection Sort", "Insertion Sort", "Quick Sort", "Merge Sort"};
+    private final String[] runTimes = {"Best case: O(n^2)\nWorst case: O(n^2)", "Best case: O(n^2)\nWorst case: O(n^2)"};
+    private Sort sort;
+
 
     public MenuScreen() {
-
+        UISetUp();
+        setFrameProperties();
     }
 
-    /**
-     * Customize the GridBayLayout for better UI, experiencing.
-     * @param panel the panel that you want to add.
-     * @param component the component,
-     * @param gridx
-     * @param gridy
-     * @param gridwidth
-     * @param gridheight
-     * @param gridBagConstraints
-     */
-    private void addComp(JPanel panel, JComponent component, int gridx, int gridy, int gridwidth, int gridheight, GridBagConstraints gridBagConstraints) {
+    private void UISetUp() {
+        // Menu panel
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        Dimension dimension = new Dimension();
+        dimension.width = 250;
+        panel.setPreferredSize(dimension);
+
+        // Labels
+        JLabel algorithmsL = new JLabel("Sort Algorithms");
+        JLabel delayL = new JLabel("Delay");
+        JLabel sizeL = new JLabel("Size");
+
+        // Sort algorithm's combo box
+        JComboBox<String> sortAlgorithms = new JComboBox<>(sortAlgos);
+
+        // Buttons
+        JButton startBtn = new JButton("       Sort       ");
+        JButton shuffleBtn = new JButton("     Shuffle     ");
+
+        // Sliders
+        JSlider sizeSlid = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+
+        JSlider delaySlid = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+
+        // JTextArea
+        JTextArea algorithmsTime = new JTextArea(runTimes[0]);
+
+        // Border
+        Border innerBorder = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED), "Sort Visualizer");
+        Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        panel.setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        // algorithm Label
+        addComp(panel, algorithmsL, 0, 0, gridBagConstraints);
+
+        // sort algorithm selection
+        addComp(panel, sortAlgorithms, 0, 1, gridBagConstraints);
+
+        // sort button
+        addComp(panel, startBtn, 0, 2, gridBagConstraints);
+
+        // shuffle button
+        addComp(panel, shuffleBtn, 0, 3, gridBagConstraints);
+
+        // delay label
+        addComp(panel, delayL, 0, 4, gridBagConstraints);
+
+        // delay slider
+        addComp(panel, delaySlid, 0, 5, gridBagConstraints);
+        delaySlid.setMajorTickSpacing(20);
+        delaySlid.setPaintTicks(false);
+
+        // size of boxes label
+        addComp(panel, sizeL, 0, 6, gridBagConstraints);
+
+        // size slider selection
+        addComp(panel, sizeSlid, 0, 7, gridBagConstraints);
+        sizeSlid.setMajorTickSpacing(50);
+        sizeSlid.setPaintTicks(false);
+
+        // Comparisons done label
+        JLabel comparison = new JLabel("Comparisons done: ");
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+        panel.add(comparison, gridBagConstraints);
+
+        // Array accessed label
+        JLabel array = new JLabel("Arrays accessed: ");
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 9;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+        panel.add(array, gridBagConstraints);
+
+//        addComp(panel, algorithmsTime, 0, 10, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 10;
+        gridBagConstraints.fill = GridBagConstraints.NONE;
+        gridBagConstraints.anchor = GridBagConstraints.LINE_START;
+        panel.add(algorithmsTime, gridBagConstraints);
+
+        this.add(panel, BorderLayout.WEST);
+    }
+
+    private void setFrameProperties() {
+        this.setTitle("Sort Visualizer");
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+    // custom gridbaglayout
+    private void addComp(JPanel panel, JComponent component, int gridx, int gridy, GridBagConstraints gridBagConstraints) {
         gridBagConstraints.gridx = gridx;
         gridBagConstraints.gridy = gridy;
-        gridBagConstraints.gridwidth = gridwidth;
-        gridBagConstraints.gridheight = gridheight;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 0.1;
         gridBagConstraints.anchor = GridBagConstraints.CENTER;
         gridBagConstraints.fill = GridBagConstraints.NONE;
+        panel.add(component, gridBagConstraints);
     }
 
 }
---
+++ src/com/VinhHua/Sort.java
@@ -0,0 +1,53 @@
+package com.VinhHua;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Sort extends JPanel{
+    private final int numsOfBoxes = 50;
+    private final int shuffle = 500;
+    private Random random = new Random();
+    private boolean sorting = false;
+    private boolean shuffled = false;
+    private int[] list;
+
+    public Sort() {
+        shuffle();
+    }
+
+
+    public void start() {
+
+    }
+
+    // create a list of boxes to sort
+    public void createList() {
+        list = new int[numsOfBoxes];
+        for (int i = 0; i < numsOfBoxes; i++) {
+            list[i] = i + 1;
+        }
+    }
+
+    // randomly shuffle the list 500 times
+    public void shuffle() {
+        for (int i = 0; i < shuffle; i++) {
+            for (int j = 0; j < numsOfBoxes; j++) {
+                int rand = random.nextInt(numsOfBoxes);
+                int temp = list[i];
+                list[i] = list[rand];
+                list[rand] = temp;
+            }
+        }
+        sorting = false;
+        shuffled = true;
+    }
+
+    // TO DO
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (int i = 0; i < numsOfBoxes; i++) {
+        }
+    }
+}
